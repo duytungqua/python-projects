27bb68922cad08a3ca8379f8f0c9764bb3796bfa
duytungqua <quangduy.hcmut@gmail.com>
2025-08-27 23:35:45 +0700

    tut: python basic synstax

---
+++ basics/basics.py
@@ -0,0 +1,15 @@
+#print
+
+print("Hello, World!")
+
+# Variables and Data Types
+x = 5               # Integer
+y = 3.14            # Float
+name = "Alice"      # String
+is_student = True   # Boolean       
+
+# Lists
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])    # Accessing list element        
+fruits.append("date") # Adding an element to the list
+print(fruits)
