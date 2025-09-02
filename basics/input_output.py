03d03c238b99ba7a86c4fdb3959561867e088f26
duytungqua <quangduy.hcmut@gmail.com>
2025-09-02 23:21:53 +0700

    tut: input & output

---
+++ basics/input_output.py
@@ -0,0 +1,7 @@
+#input 
+username = input("Enter your username: ")
+print("Username is: " + username)
+#output
+age = int(input("Enter your age: "))
+print("You are " + str(age) + " years old.")
+# Review of Python Basics
