367c3a51e445e73215104c5a896cd12cca1501ec
duytungqua <quangduy.hcmut@gmail.com>
2025-09-04 00:58:13 +0700

    tut: operation

---
+++ basics/operation.py
@@ -0,0 +1,23 @@
+a = 5
+b = 10
+
+# Arithmetic Operations
+sum_result = a + b
+diff_result = b - a
+prod_result = a * b
+div_result = b / a
+mod_result = b % a
+exp_result = a ** 2
+
+print("Sum:", sum_result)
+print("Difference:", diff_result)
+print("Product:", prod_result)
+print("Division:", div_result)
+print("Modulus:", mod_result)
+print("Exponentiation:", exp_result)
+
+# Assignment Operations
+a += 2  # a = a + 2
+b *= 3  # b = b * 3 
+print("Updated a:", a)
+print("Updated b:", b)          
