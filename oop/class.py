c85c5f4fdcd2f56565e8c15a10712dad0a08de0c
duytungqua <quangduy.hcmut@gmail.com>
2025-09-06 23:51:39 +0700

    tut:oop self

--- oop/class.py
+++ oop/class.py
@@ -2,6 +2,10 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
-    def greet(self):
-        return f"Hello, my name is {self.name} and I am {self.age} years old."  
+    @staticmethod
+    def greet():
+        return f"Hello, my name is years old."  
+    
+#instance
+person1 = Person("Alice", 30)
+print(person1.greet())
