b5ecf601872c42386a19ed4f6b615a14c03379e5
duytungqua <quangduy.hcmut@gmail.com>
2025-09-07 23:13:57 +0700

    tut: type convertion

---
+++ basics/type_convertion.py
@@ -0,0 +1,8 @@
+value = input("Enter a number: ")
+print("You entered: " + value)
+#type conversion    
+number = int(value)  # Convert string to integer
+print("After conversion, number + 10 =", number + 10)
+
+# Review of Python Basics
+# int(), float(0) str(), bool() 
