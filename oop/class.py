a9afffd71c9ad7f7e666892d1c374d2bac315a7e
duytungqua <quangduy.hcmut@gmail.com>
2025-09-07 17:12:57 +0700

    tut: self keywork

--- oop/class.py
+++ oop/class.py
@@ -3,9 +3,9 @@ class Person:
         self.name = name
         self.age = age
     @staticmethod
-    def greet():
-        return f"Hello, my name is years old."  
+    def greet(name):
+        return f"Hello, my name is years old {name}."  
     
 #instance
 person1 = Person("Alice", 30)
-print(person1.greet())
+print(person1.greet("david"))
