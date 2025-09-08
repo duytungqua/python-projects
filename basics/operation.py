3722fbc97cd7dbff14c33c571fac6ffbfb52542c
duytungqua <quangduy.hcmut@gmail.com>
2025-09-08 16:45:53 +0700

    tut: operation python

--- basics/operation.py
+++ basics/operation.py
@@ -38,3 +38,15 @@ print("x or y:", x or y)
 print("not x:", not x)      
 
 # Review of Python Basics
+
+# Arithmetic
+print("Arithmetic Operations:")
+print("5 + 10 =", 5 + 10)
+a = 5
+b = 10
+print("a * b =", a * b)
+print("b / a =", b / a)
+print("b % a =", b % a)
+print("a ** 2 =", a ** 2)
+
+print("Floor Division:", a // 3)  
