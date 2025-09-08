175ed881c445e1724a55ceea60363f5a5fc7eea8
duytungqua <quangduy.hcmut@gmail.com>
2025-09-08 23:29:55 +0700

    tut: if and ternary operation

---
+++ basics/if_statement.py
@@ -0,0 +1,11 @@
+
+a = 10 
+if a > 5: 
+    print("a is greater than 5")
+elif a == 5:
+    print("a is equal to 5")
+else:
+    print("a is 5 or less")
+#ternaty operator:
+result = "Greater than 5" if a > 5 else "5 or less"
+print(result)
