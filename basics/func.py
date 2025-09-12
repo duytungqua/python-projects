91525a2b008cbcc1008dee2ba93ce9724825e5fc
duytungqua <quangduy.hcmut@gmail.com>
2025-09-12 15:24:58 +0700

    tut: pass wrong order parameter

--- basics/func.py
+++ basics/func.py
@@ -11,3 +11,9 @@ print(multi(3, 4))
 #Default parameter
 def greet(name = "Guest"):
     return f"Hello, {name}!"
+
+def create_user(name, age, active=True):
+    print(f"Name={name}, Age={age}, Active={active}")
+create_user("Alice", True, 30)   # ❌ Wrong: active=True, age=30
+create_user(name = "Alice", age = 30)  # ✅ Correct
+create_user(age = 30, name = "Alice")  # ✅ Correct
