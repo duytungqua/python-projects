cbec57857dcd0ee1464d3de92b9df9c01fa7315b
duytungqua <quangduy.hcmut@gmail.com>
2025-09-15 23:41:20 +0700

    tut: add tupple

---
+++ basics/tuple.py
@@ -0,0 +1,4 @@
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#Tuples are written with round brackets.
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
