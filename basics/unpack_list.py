e8926bf2bb72eb4014654d4a52a2a071f96966d5
duytungqua <quangduy.hcmut@gmail.com>
2025-09-22 17:39:07 +0700

    tut: unpacking and packing

--- basics/unpack_list.py
+++ basics/unpack_list.py
@@ -10,3 +10,10 @@ print(blue) #blue
 print(green) #green
 red1, blue1 = colors[0:2]
 print(red1) #red
+
+#unpacking and packing
+lst = [1, 2, 3, 4, 5]
+a, b, *new_lst = lst
+print(a)        # 1
+print(b)        # 2
+print(new_lst)  # [3, 4, 5]
