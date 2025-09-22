9faf095e15a17fc5cb2214531a4ae358cf8a0469
duytungqua <quangduy.hcmut@gmail.com>
2025-09-22 17:37:23 +0700

    tut: unpack list

---
+++ basics/unpack_list.py
@@ -0,0 +1,12 @@
+#unpack list
+colors = ['red', 'blue', 'green']
+print(colors[0]) #red
+print(colors[1]) #blue
+print(colors[2]) #green
+#unpack list to variables
+red, blue, green = colors
+print(red) #red
+print(blue) #blue
+print(green) #green
+red1, blue1 = colors[0:2]
+print(red1) #red
