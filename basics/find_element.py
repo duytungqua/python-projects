2ae6f10c2dade13b3e992b77c5852930f2102696
duytungqua <quangduy.hcmut@gmail.com>
2025-10-14 23:52:36 +0700

    tut: add interable

--- basics/find_element.py
+++ basics/find_element.py
@@ -4,3 +4,14 @@ cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
 def city_finder(city_idx):
     result = print("Finding city:", city_idx)
     print("City found:", cities[city_idx])
+    print("Index of city:", cities.index("Los Angeles"))
+
+str = 'Iterables'
+for char in str:
+    print(char)
+
+colors = ['red', 'green', 'blue']
+colors_iter = iter(colors)
+
+color = next(colors_iter)
+print(color)
