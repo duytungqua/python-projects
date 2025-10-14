ee79b3d305df57f4a3b2b91d0c9acd2c742dd9c1
duytungqua <quangduy.hcmut@gmail.com>
2025-10-14 23:46:26 +0700

    tut: find element in array

---
+++ basics/find_element.py
@@ -0,0 +1,6 @@
+#array 
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+#find element in array
+def city_finder(city_idx):
+    result = print("Finding city:", city_idx)
+    print("City found:", cities[city_idx])
