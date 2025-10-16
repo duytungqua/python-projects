3868a98cb8eb8a8452f56f11506df9874dc4e5be
duytungqua <quangduy.hcmut@gmail.com>
2025-10-16 23:48:32 +0700

    tut: using python list with tuple

--- basics/transform_list_with_map.py
+++ basics/transform_list_with_map.py
@@ -17,3 +17,13 @@ bonuses = [100, 200, 300]
 def apply_bonus(bonus):
     return bonus * 1.1
 iterator = map(apply_bonus, bonuses)    
+
+#tuple
+points = (1, 2, 3)
+def double_point(point):
+    return point * 2
+points_iterator = map(double_point, points)
+updated_points = list(points_iterator)
+for updated_point in updated_points:
+    print("Updated Point:", updated_point)
+# Using map to transform a list
